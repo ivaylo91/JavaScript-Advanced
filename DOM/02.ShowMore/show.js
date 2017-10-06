5db6fef5ab00eb8addb19633d14d80c71722f4d9
ivaylo91 <ipenev91@gmail.com>
2017-10-06 15:43:40 +0300

    Show and hide text when click button

---
+++ DOM/02.ShowMore/show.js
@@ -0,0 +1,13 @@
+function showText(){
+    "use strict";
+
+    let text = document.getElementById('text');
+
+    if(text.style.display === 'none'){
+
+        text.style.display = 'block';
+    } else {
+
+        text.style.display = 'none';
+    }
+}
