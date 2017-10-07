01d4ff73b1fefec262bf279dba0d26a1a0388150
ivaylo91 <ipenev91@gmail.com>
2017-10-07 20:55:03 +0300

    Extract parenthesis from paragraph

---
+++ DOM/06.ExtractParenthesis/extract.js
@@ -0,0 +1,18 @@
+let text = extract('content');
+
+function extract(elementId) {
+
+    let para = document.getElementById(elementId).textContent;
+
+    let pattern = /\(([^)]+)\)/g;
+
+    let result = [];
+
+    let match = pattern.exec(para);
+
+    while (match) {
+        result.push(match[1]);
+        match = pattern.exec(para);
+    }
+    return result.join("; ");
+}
