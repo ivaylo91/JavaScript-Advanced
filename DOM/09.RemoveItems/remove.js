84db06ff3932362e475f45d9933c9a56b462c5e6
ivaylo91 <ipenev91@gmail.com>
2017-10-11 16:43:47 +0300

    add and remove items from list

--- DOM/09.RemoveItems/remove.js
+++ DOM/09.RemoveItems/remove.js
@@ -2,17 +2,16 @@ function addItems() {
     let text = document.getElementById("newValue").value;
     let list = document.getElementById("items");
     let li = document.createElement("li");
-    let span = document.createElement('span');
-    span.innerHTML = "<a href='#'>[Delete]</a>";
-    span.firstChild.addEventListener('click', deleteItem);
+    let deleteLink = document.createElement('a');
+    deleteLink.textContent = '[Delete]';
+    deleteLink.href = "#";
+    deleteLink.addEventListener('click', deleteItem);
     li.appendChild(document.createTextNode(text + " "));
-    li.appendChild(span);
+    li.appendChild(deleteLink);
     list.appendChild(li);
     document.getElementById("newValue").value = '';
 
     function deleteItem() {
-        let li = this.parentNode.parentNode;
-        let ul = li.parentNode;
-        ul.removeChild(li);
+        document.getElementById('items').removeChild(li);
     }
 }
