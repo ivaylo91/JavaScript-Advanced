7a56afc239873ab00b176a32f1704e6a86f56405
ivaylo91 <ipenev91@gmail.com>
2017-10-24 19:18:31 +0300

    Extract list items with jQuery

--- jQuery/01.TextFromList/extract-items.js
+++ jQuery/01.TextFromList/extract-items.js
@@ -1,7 +1,8 @@
 function extractText(){
-    let result = [];
+    /*let result = [];
 
     $('#items').find('li').each((index,element)=>result.push(element.textContent));
-    $('#result').text(result.join(','));
+    $('#result').text(result.join(','));*/
 
+    $('#result').text($('#items').find('li').toArray().map(e=>e.textContent).join(','))
 }
