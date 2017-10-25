2a74b94832f0316557218b3b5a3b60863528a2e7
ivaylo91 <ipenev91@gmail.com>
2017-10-25 12:42:26 +0300

    search values in list

--- jQuery/02.SearchInList/search.js
+++ jQuery/02.SearchInList/search.js
@@ -1,8 +1,7 @@
 function search(){
 
     let pattern = $('#searchText').val();
-    let matched = $('ul#towns').find(`li:contains(${pattern})`);
-    matched.css('font-weight','bold');
+    $('ul#towns').find(`li:contains(${pattern})`).css('font-weight','bold');
     $('#towns').find(`li:not(:contains(${pattern}))`).css('font-weight','');
     $('#result').text(matched.length + " matches found");
 }
