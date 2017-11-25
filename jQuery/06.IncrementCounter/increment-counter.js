82ca46193c4dc850b226ac287240614c237fccc2
ivaylo91 <ipenev91@gmail.com>
2017-11-25 20:15:56 +0200

    Create Book Generator with select and deselect functionality.

--- jQuery/06.IncrementCounter/increment-counter.js
+++ jQuery/06.IncrementCounter/increment-counter.js
@@ -1,7 +1,7 @@
 function increment(selector) {
-    let textArea = $('<textarea class="counter"; disabled>0</textarea>')
-    let buttonIncr = $('<button class="btn"; id="incrementBtn">Increment</button>')
-    let buttonAdd = $('<button class="btn"; id="addBtn">Add</button>')
+    let textArea = $('<textarea class="counter"; disabled>0</textarea>');
+    let buttonIncr = $('<button class="btn"; id="incrementBtn">Increment</button>');
+    let buttonAdd = $('<button class="btn"; id="addBtn">Add</button>');
     let list = $('<ul class="results">');
     $(selector).append(textArea);
     $(selector).append(buttonIncr);
