812951b2c36355a65e48d3c561cb2742c9666de2
ivaylo91 <ipenev91@gmail.com>
2017-12-07 22:44:36 +0200

    Format currency function

---
+++ 03.Advanced-Functions/02.FormatCurrency.js
@@ -0,0 +1,9 @@
+function formatCurrency(separator, symbol, symbolFirst, value) {
+
+    let result = Math.trunc(value) + separator;
+    result += value.toFixed(2).substr(-2, 2);
+    if (symbolFirst) return symbol + ' ' + result;
+    else return result + ' ' + symbol;
+}
+
+console.log(formatCurrency(",", 'лв', false, 1.60));
