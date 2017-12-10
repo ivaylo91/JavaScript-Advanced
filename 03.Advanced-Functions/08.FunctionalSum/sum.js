de34003e59c1d7e9c7970f3c9050cf7779c42812
ivaylo91 <ipenev91@gmail.com>
2017-12-10 21:34:02 +0200

    add function return sum, patient condition after calc personal bmi

---
+++ 03.Advanced-Functions/08.FunctionalSum/sum.js
@@ -0,0 +1,13 @@
+let add = (function () {
+    let sum = 0;
+
+    return function add(number) {
+        sum += number;
+        add.toString = function () {
+            return sum;
+        };
+        return add;
+    }
+})();
+
+console.log(add(1)(6)(-3).toString());
