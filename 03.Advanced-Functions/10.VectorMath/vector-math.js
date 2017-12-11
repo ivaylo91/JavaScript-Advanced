5e18ee0f31e4da76f57431bcad8b550f4f5b6996
ivaylo91 <ipenev91@gmail.com>
2017-12-11 10:23:08 +0200

    Calculate Vectors

--- 03.Advanced-Functions/10.VectorMath/vector-math.js
+++ 03.Advanced-Functions/10.VectorMath/vector-math.js
@@ -9,7 +9,7 @@ let solve = (function () {
 
             return [vec1[0] * scalar, vec1[1] * scalar];
         },
-        lenght: function (vec1) {
+        length: function (vec1) {
 
             return Math.sqrt(Math.pow(vec1[0], 2) + Math.pow(vec1[1], 2));
         },
@@ -25,6 +25,6 @@ let solve = (function () {
 
 console.log(solve.add([1, 1], [1, 0])); // [1+1,1+0] = [2,1]
 console.log(solve.multiply([3.5, -2], 2)); // [3.5*2, (-2)*2] = [7,-4]
-console.log(solve.lenght([3, -4])); // sqrt(3*3 + (-4)*(-4)) = 5
+console.log(solve.length([3, -4])); // sqrt(3*3 + (-4)*(-4)) = 5
 console.log(solve.dot([1, 0], [0, -1])); // 1*0 + 0*(-1) = 0
 console.log(solve.cross([3, 7], [1, 0])); // 3*0 - 7*1
