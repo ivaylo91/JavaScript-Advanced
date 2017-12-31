d8755e8ed9d469cd7b3dc6356319d0c5bf503257
ivaylo91 <ipenev91>
2017-12-31 21:25:01 +0200

    Unit Testing for Symmetric Array.

---
+++ 05.Unit-Testing/05.Symmetric-Array/test/tests.js
@@ -0,0 +1,25 @@
+let isSymmetric = require('../symmetric-array').isSymmetric;
+
+let expect = require('chai').expect;
+
+describe("Check symmetry", function () {
+    describe("General Test", function () {
+        it("should be a function", function () {
+            expect(typeof isSymmetric).to.equal('function');
+        });
+    });
+    describe("Value Tests", function () {
+        it('should return true', function () {
+            expect(isSymmetric([1, 2, 3, 3, 2, 1])).to.be.true;
+        });
+        it('should return false', function () {
+            expect(isSymmetric([1, 2, 3, 4, 2, 1])).to.be.false;
+        });
+        it('should return true for [1,2,3,2,1]', function () {
+            expect(isSymmetric([1, 2, 3, 2, 1])).to.be.true;
+        });
+        it('should return false for [1,2,4,1]', function () {
+            expect(isSymmetric([1, 2, 3, 4, 1])).to.be.false;
+        });
+    });
+});
