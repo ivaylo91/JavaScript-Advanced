045f39ec91d6264136fb58848745b097c3d7f2f7
ivaylo91 <ipenev91>
2017-12-31 17:44:08 +0200

    Unit Testing Sumator using Mocha

--- 05.Unit-Testing/04.Sum/sum.js
+++ 05.Unit-Testing/04.Sum/sum.js
@@ -15,4 +15,7 @@ describe("Test sumator", function () {
     it("Should return 0 for []", function () {
         expect(sum([])).to.equal(0);
     });
+    it("Should return 3 for['1',2])", function () {
+        expect(sum(['1', 2])).to.equal(3);
+    });
 });
