6ae9a3f94c9e725f9ea4cf94522cdd1dd1577f92
ivaylo91 <ipenev91>
2018-01-02 16:40:43 +0200

    Test Calculator Maker

--- 05.Unit-Testing/07.Add-Subtract-Module/tests/add-subtract-module.test.js
+++ 05.Unit-Testing/07.Add-Subtract-Module/tests/add-subtract-module.test.js
@@ -3,8 +3,14 @@ let createCalculator = require('../add-substract-module').createCalculator;
 let expect = require('chai').expect;
 
 describe("Calculator maker", () => {
+
+    let calculator;
+
+    beforeEach(() => {
+        calculator = createCalculator();
+    });
     it("should return an object", () => {
-        let calculator = createCalculator();
         expect(typeof  calculator).to.equal("object");
     });
+
 });
