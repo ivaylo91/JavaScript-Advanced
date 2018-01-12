2f5d072ba228625053ecaad97ee98907c91b7348
ivaylo91 <ipenev91@gmail.com>
2018-01-12 10:59:57 +0200

    print Ticket and sort by criteria - status or destination

--- 06.ClassesAndClassMembers/08.Tickets/tickets.js
+++ 06.ClassesAndClassMembers/08.Tickets/tickets.js
@@ -6,7 +6,7 @@ class Ticket {
         this.status = status;
     }
 
-    printTickets(arr,criteria) {
+    printTickets(arr, criteria) {
         let result = [];
 
         for (let values of arr) {
@@ -20,4 +20,5 @@ class Ticket {
 }
 
 let ticket = new Ticket();
-console.log(ticket.printTickets(['Philadelphia|94.20|available', 'New York City|95.99|available', 'New York City|95.99|sold', 'Boston|126.20|departed'], 'destination'));;
+console.log(ticket.printTickets(['Philadelphia|94.20|available', 'New York City|95.99|available',
+    'New York City|95.99|sold', 'Boston|126.20|departed'], 'destination'));
