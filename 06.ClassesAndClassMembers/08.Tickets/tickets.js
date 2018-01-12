7c4d9d24fd580c256ff6ec2d307b26ce14870952
ivaylo91 <ipenev91@gmail.com>
2018-01-12 10:59:14 +0200

    print Ticket and sort by criteria - status or destination

--- 06.ClassesAndClassMembers/08.Tickets/tickets.js
+++ 06.ClassesAndClassMembers/08.Tickets/tickets.js
@@ -1,23 +1,23 @@
-function printTickets(arr, criteria) {
+class Ticket {
 
-    class Ticket {
+    constructor(destination, price, status) {
+        this.destination = destination;
+        this.price = price;
+        this.status = status;
+    }
 
-        constructor(destination, price, status) {
-            this.destination = destination;
-            this.price = price;
-            this.status = status;
+    printTickets(arr,criteria) {
+        let result = [];
+
+        for (let values of arr) {
+            let [destination, price, status] = values.split('|');
+            let ticket = new Ticket(destination, price, status);
+            result.push(ticket);
         }
+
+        return result.sort((a, b) => a[criteria] > b[criteria]);
     }
-
-    let result = [];
-
-    for (let values of arr) {
-        let [destination, price, status] = values.split('|');
-        let ticket = new Ticket(destination, price, status);
-        result.push(ticket);
-    }
-
-    return result.sort((a, b) => a[criteria] > b[criteria]);
 }
 
-console.log(printTickets(['Philadelphia|94.20|available', 'New York City|95.99|available', 'New York City|95.99|sold', 'Boston|126.20|departed'], 'destination'));
+let ticket = new Ticket();
+console.log(ticket.printTickets(['Philadelphia|94.20|available', 'New York City|95.99|available', 'New York City|95.99|sold', 'Boston|126.20|departed'], 'destination'));;
